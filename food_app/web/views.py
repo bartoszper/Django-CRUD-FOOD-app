2d1330abb590fc673162024012112ea81599d2e2
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-22 22:21:16 +0100

    Create 'create_item' function

--- food_app/web/views.py
+++ food_app/web/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Item
+from .forms import ItemForm
 
 # Create your views here.
 
@@ -19,5 +20,11 @@ def detail(request, item_id):
     return render(request, 'detail.html', context)
 
 def create_item(request):
-    
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request, 'item-form.html', {'form':form})
 
