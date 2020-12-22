e23b4d09f70a98ecd4bcf5994df1c8bf0eb94357
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-22 18:57:07 +0100

    Update models.py

--- food_app/web/models.py
+++ food_app/web/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
     item_name = models.CharField(max_length=50)
     item_desc = models.CharField(max_length=50)
     item_price = models.IntegerField(blank=True)
+    item_image = models.CharField(max_length=500, default='https://icon-library.com/images/placeholder-image-icon/placeholder-image-icon-21.jpg')
 
     def __str__(self):
         return self.item_name
