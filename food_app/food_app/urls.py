a4c90758c472f0f6ff14516c14dd689d477938aa
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-23 15:37:22 +0100

    Add authentication view

--- food_app/food_app/urls.py
+++ food_app/food_app/urls.py
@@ -2,10 +2,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.contrib.auth import views as authentication_view
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('web.urls')),
-    path('register/',user_views.register, name = 'register')
+    path('register/',user_views.register, name = 'register'),
+    path('login/', authentication_view.LoginView.as_view(), name = 'login')
 ]
