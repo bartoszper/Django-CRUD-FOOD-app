18c1c9392745810d7881fac84db6e129d68739b6
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-23 14:40:57 +0100

    Update register func

--- food_app/users/views.py
+++ food_app/users/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'user/register.html', {'form':form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Welcome {username}, you account has been created')
+            return redirect('food:index')
+    return render(request, 'register.html', {'form':form})
