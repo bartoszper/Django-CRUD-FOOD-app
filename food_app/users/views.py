0fbc91824124955f03770874418d33edcbbe204b
Bartosz Perenc <bartosz.perenc@gmail.com>
2020-12-23 15:20:53 +0100

    Create RegisterForm

--- food_app/users/views.py
+++ food_app/users/views.py
@@ -7,6 +7,7 @@ def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST or None)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Welcome {username}, you account has been created')
             return redirect('food:index')
